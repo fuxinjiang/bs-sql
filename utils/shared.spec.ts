aa772423f4b627c1c000fb6b30b73061ea3b294d
WumaCoder <15804854160@163.com>
2023-09-22 15:05:04 +0800

    feat: 支持字段里输入括号

--- utils/shared.spec.ts
+++ utils/shared.spec.ts
@@ -3,12 +3,14 @@ import { sqlFieldReplace } from "./shared";
 describe("shared.ts", () => {
   it("should sqlFieldReplace", () => {
     expect(
-  sqlFieldReplace(
-    `select 模块, count(模块) as c from ? where 模块='模块' group by 模块 order by c desc`,
-    {
-      模块: "mod"
-    }
-  )
-).toMatchInlineSnapshot(`"select mod, count(mod) as c from ? where mod='模块' group by mod order by c desc"`);
+      sqlFieldReplace(
+        `select 模块, count(模块) as c from ? where 模块='模块' group by 模块 order by c desc`,
+        {
+          模块: "mod",
+        }
+      )
+    ).toMatchInlineSnapshot(
+      `"select mod, count(mod) as c from ? where mod='模块' group by mod order by c desc"`
+    );
   });
 });
--- utils/shared.ts
+++ utils/shared.ts
@@ -1,9 +1,19 @@
 export function sqlFieldReplace(sql: string, replace: Record<string, string>) {
   const keys = Object.keys(replace).sort((a, b) => b.length - a.length);
-  const reg = /([^\w,()?= ]+)/gm;
+  const reg = /([^\s,?= ]+)/gm;
   return sql.replace(reg, (match, p1) => {
     const trimmedMatch = match.trim();
-    const replacement = replace[trimmedMatch];
+    const replacement = replace[trimmedMatch] ?? replace[p1];
+    if (
+      !replacement &&
+      trimmedMatch.includes("(") &&
+      trimmedMatch.includes(")")
+    ) {
+      return trimmedMatch.replace(/\((.+)\)/gm, (match, p1) => {
+        const replacement = replace[p1];
+        return `(${replacement || match})`;
+      });
+    }
     return replacement || match;
   });
 }
